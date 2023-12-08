4954d1d40e65333137c71cdad9c3c44bb21b1b62
MaxDomingoUPF <115103338+MaxDomingoUPF@users.noreply.github.com>
2023-12-08 13:54:10 +0100

    Add files via upload

--- myTeam.py
+++ myTeam.py
@@ -6,29 +6,24 @@ from contest.captureAgents import CaptureAgent
 from contest.game import Directions,Actions
 from contest.util import nearestPoint
 
-#################
-# Team creation #
-#################
+# Team creation 
+
 def create_team(firstIndex, secondIndex, isRed, first='AStarOfTheShow', second='AStarOfTheShow'):
     return [eval(first)(firstIndex), eval(second)(secondIndex)]
 
-##########
-# Agents #
-##########
 
-
+#Agent for both
 class AStarOfTheShow(CaptureAgent):
-    class Mode:
+    class Mode: #helper class to select mode
         Attack = 0
         Defend = 1
         Retreat = 2
-        Cutpath = 3
         Capsule = 4
 
     def __init__(self, index, time_for_computing=.1):
         super().__init__(index, time_for_computing)
         self.start = None
-        
+    #init variables
     def register_initial_state(self, game_state):
         self.start = game_state.get_agent_position(self.index)
         CaptureAgent.register_initial_state(self, game_state)
@@ -43,59 +38,8 @@ class AStarOfTheShow(CaptureAgent):
         self.maxHeld = 4
         
 
-    def food_in_state(self,game_state):
-        return len(self.get_food(game_state).as_list())
-    
-    def DecisionTree(self,game_state):
-        Agent2 = self.get_team(game_state)[0]
-        Agent2_state = game_state.get_agent_state(Agent2)
-        Agent1 = self.get_team(game_state)[1]
-        Agent1_state = game_state.get_agent_state(Agent1)
-        carring = game_state.get_agent_state(self.index).num_carrying
-        defending = self.allies_defending(game_state)
-        invaders = self.invaders_present(game_state)
-        if Agent1 == self.index:
 
-            if (self.initial_time-game_state.data.timeleft) < 150:        
-                return self.Mode.Retreat
-            
-            if Agent1_state.scared_timer == 0:        
-                return self.Mode.Defend
-            
-            if  Agent1_state.scared_timer > 0 :
-                return self.Mode.Attack
-            
-
-        
-        if Agent2 == self.index:
-            
-            if carring  > self.maxHeld  and self.scared_time_remaining(game_state) < 10 or (game_state.data.timeleft < 200 and carring > 1):
-                return self.Mode.Retreat
-            
-            capsules = self.get_capsules(game_state)
-
-            if capsules:
-                return self.Mode.Capsule
-
-            if self.scared_time_remaining(game_state) > 10:
-                return self.Mode.Attack
-
-            if not self.allies_defending(game_state) and self.invaders_present(game_state):
-                return self.Mode.Defend
-            
-            if self.food_in_state(game_state)+3 < len(self.get_food_you_are_defending(game_state).as_list()):
-                return self.Mode.Defend
-
-            return self.Mode.Attack
-
-
-        
-        return self.mode  #default: keep doing your thing  
-    
-    def invaders_present(self, game_state):
-        enemies = [game_state.get_agent_state(i) for i in self.get_opponents(game_state)]
-        invaders = [enemy for enemy in enemies if enemy.is_pacman and enemy.get_position() is not None]
-        return len(invaders) > 0
+###################################### MAIN STRUCTURE, A*LIKE ##########################################
 
     def choose_action(self, game_state):
         # Collect legal moves and successor states
@@ -127,18 +71,56 @@ class AStarOfTheShow(CaptureAgent):
         else:
             return successor
 
-    def min_distance_from_list(self, lst, position):
-        distances = [self.get_maze_distance(position, element) for element in lst]
-        min_distance = min(distances)
-        min_index = distances.index(min_distance)
-        return min_distance, min_index
-    def min_distance_from_list(self, lst, position):
-        distances = [self.get_maze_distance(position, element) for element in lst]
-        min_distance = min(distances)
-        min_index = distances.index(min_distance)
-        return min_distance, min_index
+ 
+################################ DECISION TREE, SELECT MODE #######################################
+    def DecisionTree(self,game_state):
+        Agent2 = self.get_team(game_state)[0]
+        Agent2_state = game_state.get_agent_state(Agent2)
+        Agent1 = self.get_team(game_state)[1]
+        Agent1_state = game_state.get_agent_state(Agent1)
+        carring = game_state.get_agent_state(self.index).num_carrying
+        defending = self.allies_defending(game_state)
+        invaders = self.invaders_present(game_state)
+        if Agent1 == self.index:
+
+            if (self.initial_time-game_state.data.timeleft) < 150:        
+                return self.Mode.Retreat        #get into position
+            
+            if  Agent1_state.scared_timer > 0 or not invaders:
+                return self.Mode.Attack     #while you are scared try to atack for value 1
+            
+            if carring  >= 2  and self.scared_time_remaining(game_state) < 10 or (game_state.data.timeleft < 200 and carring > 1):
+                return self.Mode.Retreat   #get back home 
+            return self.Mode.Defend #default defend
+            
+
+        
+        if Agent2 == self.index:
+            
+            if carring  > self.maxHeld  and self.scared_time_remaining(game_state) < 10 or (game_state.data.timeleft < 200 and carring > 1):
+                return self.Mode.Retreat
+            
+            capsules = self.get_capsules(game_state)
+
+            if capsules:
+                return self.Mode.Capsule
+
+            if self.scared_time_remaining(game_state) > 10:
+                return self.Mode.Attack
+
+            if not self.allies_defending(game_state) and self.invaders_present(game_state):
+                return self.Mode.Defend
+            
+            if self.food_in_state(game_state)+3 < len(self.get_food_you_are_defending(game_state).as_list()):
+                return self.Mode.Defend
+
+            return self.Mode.Attack
+
+
+        
+        return self.mode  #default: keep doing your thing  
     
-    
+   ################################# HEURISTIC FUNCTIONS ######################################## 
     def close_food_heuristic(self, game_state, action):
         my_score = 0
         successor = self.get_successor(game_state, action)
@@ -187,36 +169,26 @@ class AStarOfTheShow(CaptureAgent):
                 my_score -= 10000
         return my_score-distance_to_home[0]  
     
-    def safe_zone_limit(self,game_state):
-        limit = []
-        no_walls = []
-        if self.red:
-            i = self.width_half - 1
-        else:
-            i = self.width_half + 1
-        limit = [(i,float(j)) for j in  range(self.height)]
-        for i in limit:
-            if not game_state.data.layout.walls[int(i[0])][int(i[1])]:
-                no_walls.append(i)
-        return no_walls
+
     
     def heuristic_defend(self, game_state, action):
         successor = self.get_successor(game_state,action) 
         my_state = successor.get_agent_state(self.index)
         position = my_state.get_position()
         my_score = 0.0
-
-        # Evaluate the distance to invaders (enemy Pac-Man)
         enemies = [successor.get_agent_state(i) for i in self.get_opponents(successor)]
         invaders = [enemy for enemy in enemies if enemy.is_pacman and enemy.get_position() is not None]
         if invaders: 
             invader_positions = [invader.get_position() for invader in invaders]
             closest_invader_distance = self.min_distance_from_list(invader_positions, position)[0]
 
-                # Encourage the agent to chase the closest invader
             my_score -= pow(closest_invader_distance,10)  # Positive score to chase invaders
-        if position[0] < self.width_half:
-            my_score += 10
+        if self.red:
+            if position[0] < self.width_half:
+                my_score += 10      #don't leave your side
+        else: 
+            if position[0] > self.width_half:
+                my_score += 10      #don't leave your side            
         return my_score
 
 
@@ -243,7 +215,7 @@ class AStarOfTheShow(CaptureAgent):
 
         return my_score
 
-
+####################################### HELPER FUNCTIONS ###############################
 
     def invaders_present(self, game_state):
         enemies = [game_state.get_agent_state(i) for i in self.get_opponents(game_state)]
@@ -260,4 +232,29 @@ class AStarOfTheShow(CaptureAgent):
         allies_defending = [ally for ally in allies if not ally.is_pacman and ally.get_position() is not None]
         return len(allies_defending) > 1
     
+    def food_in_state(self,game_state):
+        return len(self.get_food(game_state).as_list())    
     
+    def safe_zone_limit(self,game_state):
+        limit = []
+        no_walls = []
+        if self.red:
+            i = self.width_half - 1
+        else:
+            i = self.width_half + 1
+        limit = [(i,float(j)) for j in  range(self.height)]
+        for i in limit:
+            if not game_state.data.layout.walls[int(i[0])][int(i[1])]:
+                no_walls.append(i)
+        return no_walls
+    
+    def min_distance_from_list(self, lst, position):
+        distances = [self.get_maze_distance(position, element) for element in lst]
+        min_distance = min(distances)
+        min_index = distances.index(min_distance)
+        return min_distance, min_index
+    
+    def invaders_present(self, game_state):
+        enemies = [game_state.get_agent_state(i) for i in self.get_opponents(game_state)]
+        invaders = [enemy for enemy in enemies if enemy.is_pacman and enemy.get_position() is not None]
+        return len(invaders) > 0
